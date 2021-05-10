5fee3e3f8e7f228ef44136ed91575ba4f0b60c51
danilsocol <danilsocol-2002@mail.ru>
2021-05-11 03:32:28 +0500

    Draw board

---
+++ Chess.WPF/CreateCell.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Chess_3._0;
+
+
+namespace Chess.WPF
+{
+    class CreateCell
+    {
+        private int x, y;
+        private bool color;
+
+        public CreateCell(int xXx, int yYy)
+        {
+            x = xXx;
+            y = yYy;
+
+            if ((x + y) % 2 == 1)
+                color = true;
+            else
+                color = false;
+        }
+
+        public void OutputCellData(Canvas canvas, ModelBoard board, int i, int j)
+        {
+            var cellSize = (canvas.ActualHeight + canvas.ActualWidth) / 20;
+
+            var cell = new Button
+            {
+                Width = cellSize,
+                Height = cellSize,
+                Name = $"x{y}y{x}"
+            };
+
+            if (color)
+                cell.Background = Brushes.Gray;
+            else
+                cell.Background = Brushes.White;
+
+            if (board.cell[y - 1, x - 1].Role != Roles.V)
+            {
+                cell.Content = Convert.ToString(board.cell[y - 1, x - 1].Role);
+
+                if (board.cell[y - 1, x - 1].Color == Chess_3._0.Colors.White)
+                {
+                    cell.Foreground = Brushes.Red;
+                    cell.Foreground = Brushes.Red;
+                }
+                else
+                {
+                    cell.Foreground = Brushes.Blue;
+                    cell.Foreground = Brushes.Blue;
+                }
+
+                //cell.Click += new RoutedEventHandler(NewGame.OnFigurePress);
+            }
+
+            cell.FontSize = cellSize / 4;
+
+            canvas.Children.Add(cell);
+
+            Canvas.SetTop(cell, x * cellSize);
+            Canvas.SetLeft(cell, y * cellSize);
+        }
+
+
+        private static char ch = 'A';
+        public void OutputCellRamk(Canvas canvas)
+        {
+            var cellSize = (canvas.ActualHeight + canvas.ActualWidth) / 20;
+
+            var borden = new Border();
+            var text = new TextBlock();
+            text.Text = Convert.ToString(ch);
+
+            if (ch == 'H')
+                ch = (char)(ch - 24);
+
+            ch = (char)(ch + 1);
+
+            text.Height = cellSize / 4;
+            text.Width = cellSize / 4;
+            text.FontSize = cellSize / 4;
+
+            borden.Width = cellSize;
+            borden.Height = cellSize;
+
+            borden.Child = text;
+            canvas.Children.Add(borden);
+
+            Canvas.SetTop(borden, x * cellSize);
+            Canvas.SetLeft(borden, y * cellSize);
+        }
+    }
+}
---
+++ Chess.WPF/NewGame.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Chess_3._0;
+
+namespace Chess.WPF
+{
+    public partial class NewGame : Window
+    {
+        public ModelBoard board = new ModelBoard();
+        public NewGame()
+        {
+            InitializeComponent();
+        }
+
+        private void canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            board.PlacementOfFigureNewGame();
+
+            canvas.Children.Clear();
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    CreateCell field = new CreateCell(i, j);
+
+                    if (i != 0 && j != 0)
+                    {
+                        field.OutputCellData(canvas, board, i, j);
+                    }
+                    else if (!(i == 0 && j == 0))
+                    {
+                        field.OutputCellRamk(canvas); // название метода
+                    }
+                }
+            }
+        }
+    }
+}
