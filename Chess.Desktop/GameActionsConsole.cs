17fd0679a0ae0998d98e9f70cb70b234e6d613c6
danilsocol <danilsocol-2002@mail.ru>
2021-06-01 16:14:45 +0500

    Start add console

---
+++ Chess.Desktop/GameActionsConsole.cs
@@ -0,0 +1,175 @@
+﻿using Chess_3._0;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.Desktop
+{
+    class GameActionsConsole
+    {
+
+        private static ModelBoard board { get; } = new ModelBoard();
+
+        private static int[] selectCell = { 0, 0 };
+        private static int[] pastSelectCell = { 0, 0 };
+        private static string[] movementOfTheFigures = new string[2];
+
+        public static void CreateUser(Player player)
+        {
+            do
+            {
+                player = new Player(WorkConsole.ReadName());
+                Console.Clear();
+
+            } while (player.Name.Length == 0);
+        }
+        public static void GameActions()
+        {
+
+            board.PlacementOfFigureNewGame();
+            CreateUser(ModelBoard.PlayerOne);
+            CreateUser(ModelBoard.PlayerTwo);
+
+            while (true)
+            {
+                WorkConsole.WriteField(board.cell);
+
+                SelectCell();
+
+                MakeMove(movementOfTheFigures, board.cell);
+            }
+        }
+
+        public static void MakeMove(string[] move, Cell[,] cell)
+        {
+            string start = move[0];
+
+            if (!RecognitionFigureInCell(cell, start))
+                WorkConsole.Error();
+
+            string end = move[1];
+
+            bool isCorrect = CheckCorrectOfTheMove(board, start, end);
+
+            int StartHorizontalCood = start[1] - 48;
+            int StartVerticalCoord = start[0] - 48;
+
+            int EndHorizontalCood = end[1] - 48;
+            int EndVerticalCoord = end[0] - 48;
+
+            if (isCorrect)
+            {
+                Roles pickFigure = board.cell[StartVerticalCoord, StartHorizontalCood].Role ;
+                board.cell[StartVerticalCoord, StartHorizontalCood].Role = Roles.V;
+                board.cell[StartVerticalCoord, StartHorizontalCood].Role = pickFigure;
+            }
+            else
+                WorkConsole.Error();
+        }
+
+        public static void SelectCell()
+        {
+            bool IsTheFigureSelected = true;
+
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+
+                if (key == ConsoleKey.DownArrow && selectCell[1] < 7) selectCell[1]++;
+                if (key == ConsoleKey.UpArrow && selectCell[1] > 0) selectCell[1]--;
+                if (key == ConsoleKey.RightArrow && selectCell[0] < 7) selectCell[0]++;
+                if (key == ConsoleKey.LeftArrow && selectCell[0] > 0) selectCell[0]--;
+
+                //WorkConsole.WriteChoiceCell(selectCell, pastSelectCell);
+
+
+                if (key == ConsoleKey.Enter && !IsTheFigureSelected)
+                {
+                    movementOfTheFigures[1] = $"{selectCell[0]}{selectCell[1]}";
+                    break;
+                }
+
+                //if (key == ConsoleKey.Enter && IsTheFigureSelected && RecognitionFigureInCell(cell, $"{selectCell[0]}{selectCell[1]}"))
+                //{
+                //    movementOfTheFigures[0] = $"{selectCell[0]}{selectCell[1]}";
+                //    IsTheFigureSelected = false;
+                //}
+            }
+        }
+
+        public static bool CheckCorrectOfTheMove(ModelBoard board, string start, string end)
+        {
+
+            bool isCorrect = false;
+
+            switch (board.cell[start[0] - 48, start[1] - 48].Role)
+            {
+                case Roles.H:
+                    isCorrect = IsKnightCorrect(start, end);
+                    break;
+
+                case Roles.P:
+                    isCorrect = IsPawnCorrect(start, end);
+                    break;
+
+                case Roles.R:
+                    isCorrect = IsRookCorrect(start, end);
+                    break;
+
+                case Roles.Q:
+                    isCorrect = IsQueenCorrect(start, end);
+                    break;
+
+                case Roles.K:
+                    isCorrect = IsKingCorrect(start, end);
+                    break;
+
+                case Roles.B:
+                    isCorrect = IsBishopCorrect(start, end);
+                    break;
+            }
+            return isCorrect;
+        }
+
+        public static bool IsKnightCorrect(string start, string end)
+        {
+            int dx = Math.Abs(end[0] - start[0]);
+            int dy = Math.Abs(end[1] - start[1]);
+
+            return dx + dy == 3 && dx * dy == 2;
+        }
+
+        public static bool IsPawnCorrect(string start, string end)
+        {
+            return (end[0] - start[0] == 0 && end[1] - start[1] == 1);
+        }
+
+        public static bool IsRookCorrect(string start, string end)
+        {
+            return (end[0] == start[0] && end[1] != start[1] || end[0] != start[0] && end[1] == start[1]);
+        }
+
+        public static bool IsBishopCorrect(string start, string end)
+        {
+            return (Math.Abs(start[0] - end[0]) == Math.Abs(start[1] - end[1]));
+        }
+
+        public static bool IsQueenCorrect(string start, string end)
+        {
+            return (Math.Abs(start[0] - end[0]) == Math.Abs(start[1] - end[1]) || end[0] == start[0] && end[1] != start[1] || end[0] != start[0] && end[1] == start[1]);
+        }
+
+        public static bool IsKingCorrect(string start, string end)
+        {
+            return (Math.Abs(start[0] - end[0]) <= 1 && Math.Abs(start[1] - end[1]) <= 1);
+        }
+
+        public static bool RecognitionFigureInCell(Cell[,] cell, string start)
+        {
+            if ((cell[start[0] - 48, start[1] - 48]).Role == Roles.V)
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ Chess.Desktop/Program.cs
@@ -0,0 +1,140 @@
+﻿using Chess.Desktop;
+using System;
+
+namespace Chess_3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menuSelect = 1;
+
+            WorkConsole.ReadMenu(menuSelect);
+            Selection(menuSelect);
+        }
+        static void Selection(int selectCell)
+        {
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+
+                if (key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    switch (selectCell)
+                    {
+                        case 1:
+                            GameActionsConsole.GameActions();
+                            break;
+
+                        case 2:
+
+                            break;
+
+                        case 3:
+
+                            break;
+
+                        case 4:
+
+                            break;
+                    }
+                    break;
+                }
+                if (key == ConsoleKey.W || key == ConsoleKey.UpArrow)
+                    if (selectCell != 1)
+                        selectCell -= 1;
+
+                if (key == ConsoleKey.S || key == ConsoleKey.DownArrow)
+                    if (selectCell != 4)
+                        selectCell += 1;
+
+                Console.Clear();
+                WorkConsole.ReadMenu(selectCell);
+            }
+        }
+        //public static void CreateUser(Player User)
+        //{
+        //    do
+        //    {
+        //        User.Name = WorkConsole.ReadName();
+        //        Console.Clear();
+
+        //    } while (User.Name.Length == 0);
+        //}
+        //public static void GameActions()
+        //{
+        //    int[] selectCell = { 0, 0 };
+        //    int[] pastSelectCell = { 0, 0 };
+        //    string[] movementOfTheFigures = new string[2];
+
+        //    Cell[,] board = new Cell[8, 8];
+
+        //    //while (true)
+        //    //{
+        //    //    WorkConsole.WriteField(board);
+
+        //    //   SelectCell(selectCell, board, pastSelectCell, movementOfTheFigures);
+
+        //    //    MakeMove(movementOfTheFigures, board);
+        //    //}
+        //}
+
+        //public static void MakeMove(string[] move, string[,] cell)
+        //{
+        //    string start = move[0];
+
+        //    if (!FigureMove.RecognitionFigureInCell(cell, start))
+        //        WorkConsole.Error();
+
+        //    string end = move[1];
+
+        //    bool isCorrect = FigureMove.CheckCorrectOfTheMove(cell, start, end);
+
+        //    int StartHorizontalCood = start[1] - 48;
+        //    int StartVerticalCoord = start[0] - 48;
+
+        //    int EndHorizontalCood = end[1] - 48;
+        //    int EndVerticalCoord = end[0] - 48;
+
+        //    if (isCorrect)
+        //    {
+        //        string pickFigure = cell[StartVerticalCoord, StartHorizontalCood];
+        //        cell[StartVerticalCoord, StartHorizontalCood] = null;
+        //        cell[EndVerticalCoord, EndHorizontalCood] = pickFigure;
+        //    }
+        //    else
+        //        WorkConsole.Error();
+        //}
+
+        //public static void SelectCell(int[] selectCell, string[,] cell, int[] pastSelectCell, string[] movementOfTheFigures)
+        //{
+        //    bool IsTheFigureSelected = true;
+
+        //    while (true)
+        //    {
+        //        ConsoleKey key = Console.ReadKey().Key;
+
+        //        if (key == ConsoleKey.DownArrow && selectCell[1] < 7) selectCell[1]++;
+        //        if (key == ConsoleKey.UpArrow && selectCell[1] > 0) selectCell[1]--;
+        //        if (key == ConsoleKey.RightArrow && selectCell[0] < 7) selectCell[0]++;
+        //        if (key == ConsoleKey.LeftArrow && selectCell[0] > 0) selectCell[0]--;
+
+        //        WorkConsole.WriteChoiceCell(selectCell, cell, pastSelectCell);
+
+
+        //        if (key == ConsoleKey.Enter && !IsTheFigureSelected)
+        //        {
+        //            movementOfTheFigures[1] = $"{selectCell[0]}{selectCell[1]}";
+        //            break;
+        //        }
+
+        //        if (key == ConsoleKey.Enter && IsTheFigureSelected && FigureMove.RecognitionFigureInCell(cell, $"{selectCell[0]}{selectCell[1]}"))
+        //        {
+        //            movementOfTheFigures[0] = $"{selectCell[0]}{selectCell[1]}";
+        //            IsTheFigureSelected = false;
+        //        }
+        //    }
+        //}
+    }
+}
